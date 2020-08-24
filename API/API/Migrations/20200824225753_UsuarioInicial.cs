4a95b68143888114374224623de42ec0ebd3a76a
Lucas <lucaspanetto@ucl.br>
2020-08-24 20:00:00 -0300

    Ajustes iniciais API, gerando Modelo de usuário, Criando database através do Migration (code-first)

---
+++ API/API/Migrations/20200824225753_UsuarioInicial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class UsuarioInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    IdUsuario = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(nullable: true),
+                    Nome = table.Column<string>(nullable: true),
+                    Documento = table.Column<int>(nullable: false),
+                    Status = table.Column<bool>(nullable: false),
+                    Admin = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.IdUsuario);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
---
+++ API/API/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Usuario
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdUsuario { get; set; }
+        public string Email { get; set; }
+        public string Nome { get; set; }
+        public int Documento { get; set; }
+        public bool Status { get; set; }
+        public bool Admin { get; set; }
+    }
+}
